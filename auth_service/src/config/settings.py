f29509cb3be71026ed05e2bdb1b52ff9fdf208ae
Vitali Kaliutchyk <vitalikaliutchyk@gmail.com>
2025-03-20 00:52:03 +0300

    Add Error handler settings

--- auth_service/src/config/settings.py
+++ auth_service/src/config/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'EXCEPTION_HANDLER': 'users.exceptions.custom_exception_handler'
 }
 
 SIMPLE_JWT = {
