0c03712f104999efea2f3faaa2ea80df6b1d8d23
Vitali Kaliutchyk <vitalikaliutchyk@gmail.com>
2025-03-19 12:52:20 +0300

    Reg models to admin.py

--- auth_service/src/users/admin.py
+++ auth_service/src/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    # Поля, которые будут отображаться в списке пользователей
+    list_display = ['username', 'email', 'phone', 'is_staff']
+
+    # Поля в форме редактирования пользователя
+    fieldsets = UserAdmin.fieldsets + (
+        ('Дополнительные данные', {'fields': ('phone',)}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
