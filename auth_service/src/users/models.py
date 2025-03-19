ef8bd487aceff402fd2dfc476f8bda31172f70b9
Vitali Kaliutchyk <vitalikaliutchyk@gmail.com>
2025-03-19 12:43:42 +0300

    Create models CustomUser

--- auth_service/src/users/models.py
+++ auth_service/src/users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    # Добавляем новое поле 'phone'
+    phone = models.CharField(
+        max_length=15,
+        blank=True,  # Поле необязательное
+        verbose_name='Номер телефона'  # Человекочитаемое имя
+    )
+
+    # Дополнительные поля (по желанию):
+    # profile_picture = models.ImageField(upload_to='users/')
+    # birth_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        # Настраиваем название таблицы в БД
+        db_table = 'auth_users'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username  # Отображение в админке
