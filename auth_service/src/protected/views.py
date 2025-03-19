c4570d7e81eaedabb140d1f133b40e6e67676bef
Vitali Kaliutchyk <vitalikaliutchyk@gmail.com>
2025-03-19 22:24:17 +0300

    Create Authentication via JWT

--- auth_service/src/protected/views.py
+++ auth_service/src/protected/views.py
@@ -2,16 +2,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
+
 class TestAuthView(APIView):
-    """
-    Пример защищённого API-эндпоинта.
-    Доступ только для аутентифицированных пользователей.
-    """
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    # GET-метод
+    def get(self, request, *args, **kwargs):  # Добавьте аргументы
         return Response({
             "message": f"Привет, {request.user.username}!",
             "email": request.user.email,
             "phone": request.user.phone
         })
+
+    # POST-метод
+    def post(self, request, *args, **kwargs):  # Добавьте аргументы
+        return Response({"message": "POST request allowed!"})
