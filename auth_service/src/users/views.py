2724fba47f0c0f23cb971afa5b84fd35a1b78e29
Vitali Kaliutchyk <vitalikaliutchyk@gmail.com>
2025-03-19 13:30:43 +0300

    Add views.py

--- auth_service/src/users/views.py
+++ auth_service/src/users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import CustomTokenObtainPairSerializer, UserSerializer
 
-# Create your views here.
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
