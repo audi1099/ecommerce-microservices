f7c541e3333ce0e11914eefbc5d69930bf9e34fc
Vitali Kaliutchyk <vitalikaliutchyk@gmail.com>
2025-03-19 21:02:15 +0300

    Modified settings

--- auth_service/src/config/urls.py
+++ auth_service/src/config/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users.views import CustomTokenObtainPairView, RegisterView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/register/', RegisterView.as_view(), name='register'),
 ]
